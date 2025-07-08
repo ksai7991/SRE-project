96269951b333efea0c183630c75f04a272e7dca2
Vandavasi Krishna Sai <ksai7991@gmail.com>
2025-07-08 15:03:51 +0530

    jbbsd

--- CI-Python-App-main/app.py
+++ CI-Python-App-main/app.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello Wobsbhdshooopooooooooooooooooorld newwww"}
+
+# Add metrics instrumentation
+Instrumentator().instrument(app).expose(app)
